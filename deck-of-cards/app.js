988160c9df1282c79ac22d4ac20a572c161986d9
GSLoee <gurnoorloee001@gmail.com>
2024-03-06 23:53:32 -0500

    Async/Await Promises

--- deck-of-cards/app.js
+++ deck-of-cards/app.js
@@ -1,57 +1,97 @@
-let deckID = 'le6tfee69aix'
+// let deckID = 'le6tfee69aix'
 
-axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
-    .then(res => {
-        let card = res.data.cards[0]
-        console.log(`${card.value} of ${card.suit}`)
-    })
-    .catch(err => {
-        console.log('ERROR!!!!')
-    })
+// axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+//     .then(res => {
+//         let card = res.data.cards[0]
+//         console.log(`${card.value} of ${card.suit}`)
+//     })
+//     .catch(err => {
+//         console.log('ERROR!!!!')
+//     })
 
-    function getNewDeckID() {
-        return axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-            .then(response => {
-                return response.data.deck_id;
-            })
-            .catch(error => {
-                console.log('Error fetching new deck ID:', error);
-                throw error; // Propagate the error
-            });
-    }
+//     function getNewDeckID() {
+//         return axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+//             .then(response => {
+//                 return response.data.deck_id;
+//             })
+//             .catch(error => {
+//                 console.log('Error fetching new deck ID:', error);
+//                 throw error; // Propagate the error
+//             });
+//     }
+
+async function part1(){
+    let url = 'https://deckofcardsapi.com/api/deck/oons3hlsw6y0/draw/?count=1'
+    let res = await axios.get(`${url}`)
+    let card = res.data.cards[0]
+    console.log(`${card.value} of ${card.suit}`) 
     
-    function part2() {
-        getNewDeckID()
-            .then(deckID => {
-                return axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`);
-            })
-            .then(response => {
-                const firstCard = response.data.cards[0];
-                console.log(`${firstCard.value} of ${firstCard.suit}`);
-                return response
-            })
-            .then(response => {
-                const secondCard = response.data.cards[1]
-                console.log(`${secondCard.value} of ${secondCard.suit}`)
-            })
-            .catch(error => {
-                console.log('Error:', error);
-            });
-    }
+}
     
-    part2();
+    // function part2() {
+    //     getNewDeckID()
+    //         .then(deckID => {
+    //             return axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`);
+    //         })
+    //         .then(response => {
+    //             const firstCard = response.data.cards[0];
+    //             console.log(`${firstCard.value} of ${firstCard.suit}`);
+    //             return response
+    //         })
+    //         .then(response => {
+    //             const secondCard = response.data.cards[1]
+    //             console.log(`${secondCard.value} of ${secondCard.suit}`)
+    //         })
+    //         .catch(error => {
+    //             console.log('Error:', error);
+    //         });
+    // }
+    
+    // part2();
+
+    async function part2(){
+    let url = 'https://deckofcardsapi.com/api/deck/oons3hlsw6y0/draw/?count=2'
+    let res = await axios.get(`${url}`)
+    let card = res.data.cards[0]
+    let card2 = res.data.cards[1]
+    console.log(`${card.value} of ${card.suit}`) 
+    console.log(`Second Card: ${card2.value} of ${card2.suit}`)
+    }
 
 let $btn = $('button')
 let $container = $('.card-container')
 
-$btn.on('click', function(){
-    axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/`)
-    .then(res => {
-        let cards = res.data.cards[0]
-        let card = res.data.cards[0].image
-        let angle = Math.random() * 90 - 45
-        let randomY = Math.random() * 40 - 20
-        let randomX = Math.random() * 40 - 20
+// $btn.on('click', function(){
+//     axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/`)
+//     .then(res => {
+//         let cards = res.data.cards[0]
+//         let card = res.data.cards[0].image
+//         let angle = Math.random() * 90 - 45
+//         let randomY = Math.random() * 40 - 20
+//         let randomX = Math.random() * 40 - 20
+//         $container.append(
+//             $('<img>', {
+//                 src: card,
+//                 css: {
+//                     transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+//                 }
+//             })
+//         )
+//         console.log(`${card.value} of ${card.suit}`)
+//     })
+//     .catch(err => {
+//         console.log('ERROR!!!!')
+//     })
+// })
+
+$btn.on('click', async function() {
+    let res = await axios.get('https://deckofcardsapi.com/api/deck/oons3hlsw6y0/draw/')
+    try{
+    let cards = res.data.cards[0]
+    let card = res.data.cards[0].image
+    let angle = Math.random() * 90 - 45
+    let randomY = Math.random() * 40 - 20
+    let randomX = Math.random() * 40 - 20
         $container.append(
             $('<img>', {
                 src: card,
@@ -61,8 +101,9 @@ $btn.on('click', function(){
             })
         )
         console.log(`${card.value} of ${card.suit}`)
-    })
-    .catch(err => {
-        console.log('ERROR!!!!')
-    })
-})
+        } catch(e) {
+            console.log('NO MORE CARDS')
+        }})
+
+
+    
--- number-facts/app.js
+++ number-facts/app.js
@@ -1,51 +1,102 @@
 const URL = 'http://numbersapi.com/'
 const favouriteNumber = '7'
 
-function part1(){
-    axios.get(`${URL}${favouriteNumber}?json`)
-    .then(res => {
-        console.log(res.data.text)
-    })
-    .catch(err => {
+// function part1(){
+//     axios.get(`${URL}${favouriteNumber}?json`)
+//     .then(res => {
+//         console.log(res.data.text)
+//     })
+//     .catch(err => {
+//         console.log('NO DATA')
+//     })
+// }
+
+// part1()
+
+async function part11(){
+    try {
+    let res = await axios.get(`${URL}${favouriteNumber}?json`)
+    let fact = res.data.text
+    console.log(`${fact}`)
+    } catch (e) {
         console.log('NO DATA')
-    })
+    }
+    
 }
 
-part1()
+// function part2(){
+//     let number = []
+//     for(let i=1; i<5; i++){
+//         number.push(
+//             axios.get(`${URL}${i}?json`)
+//         )
+//     }
+//     Promise.all(number)
+//     .then(numberArr => {
+//     for (res of numberArr) {
+//         console.log(res.data.text)
+//     }
+//     })
+//     .catch(err => console.log(err))
+// }
 
-function part2(){
-    let number = []
-    for(let i=1; i<5; i++){
-        number.push(
-            axios.get(`${URL}${i}?json`)
-        )
+// part2()
+
+async function part22() {
+    let randomNumber = Math.floor(Math.random() * 101);
+    let randomNumber2 = Math.floor(Math.random() * 101);
+    let randomNumber3 = Math.floor(Math.random() * 101);
+    let randomNumber4 = Math.floor(Math.random() * 101);
+    try {
+        let responses = await Promise.all([
+            axios.get(`${URL}${randomNumber}?json`),
+            axios.get(`${URL}${randomNumber2}?json`),
+            axios.get(`${URL}${randomNumber3}?json`),
+            axios.get(`${URL}${randomNumber4}?json`)
+        ]);
+
+        responses.forEach((res, index) => {
+            console.log(`Response ${index + 1}:`, res.data.text);
+        });
+    } catch (error) {
+        console.log('Error:', error);
     }
-    Promise.all(number)
-    .then(numberArr => {
-    for (res of numberArr) {
-        console.log(res.data.text)
-    }
-    })
-    .catch(err => console.log(err))
 }
 
-part2()
+part22();
 
-function part3(){
-    let number = []
+// function part3(){
+//     let number = []
+//     for (let i = 0; i < 4; i++){
+//         number.push(
+//             axios.get(`${URL}${7}?json`)
+//         )
+//     }
+//     Promise.all(number)
+//     .then(numberArr => {
+//     for (res of numberArr) {
+//         console.log(res.data.text)
+//     }
+//     })
+//     .catch(err => console.log(err))
+// }
+
+
+// part3()
+
+async function part33(){
+    let number = [];
     for (let i = 0; i < 4; i++){
         number.push(
-            axios.get(`${URL}${7}?json`)
-        )
+            axios.get(`${URL}${favouriteNumber}?json`)
+        );
     }
-    Promise.all(number)
-    .then(numberArr => {
-    for (res of numberArr) {
-        console.log(res.data.text)
+    try {
+        let numberArr = await Promise.all(number);
+        for (let res of numberArr) {
+            console.log(res.data.text);
+        }
+    } catch (error) {
+        console.log('Error:', error);
     }
-    })
-    .catch(err => console.log(err))
 }
-
-
-part3()
